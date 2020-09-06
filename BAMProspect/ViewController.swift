749dd5b50533662a5f6518046a14d4bf454f2f6b
Nicolas Masson <nicolas.masson@ecojoko.com>
2020-09-06 15:04:24 +0200

    Initial Commit

---
+++ BAMProspect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BAMProspect
+//
+//  Created by Nicolas Masson on 06/09/2020.
+//  Copyright © 2020 BAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
